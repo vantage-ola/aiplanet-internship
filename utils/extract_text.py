6449f4896d2fffcb0e69e15952f03adba1b33c5e
vantage-ola <vantageola4@gmail.com>
2024-05-20 09:46:45 +0100

    finish-extracting-processing

--- utils/extract_text.py
+++ utils/extract_text.py
@@ -8,15 +8,19 @@ def extract_text(input_doc_path='input_data/sample.pdf'):
     doc = pymupdf.open(input_doc_path) # open a document
     doc_name = os.path.splitext(os.path.basename(input_doc_path))[0]   #extract document name from document path
     
-    out = open(f"output_text/{doc_name}.txt", "wb") # create a text output
+    #out = open(f"output_text/{doc_name}.txt", "wb") # create a text output
 
+    extracted_text = ""
     for page in doc: # iterate the document pages
         
         text = unidecode(page.get_text()) #some text in unicode(strange symbols), represent in ASCII
-        encoded_text = text.encode("utf8") # get plain text (is in UTF-8)
-        
-        out.write(encoded_text) # write text of page
-        out.write(bytes((12,))) # write page delimiter (form feed 0x0C)
-    out.close()
-    
+        #encoded_text = text.encode("utf8") # get plain text (is in UTF-8)
+        extracted_text += text + "\n"
+        #out.write(encoded_text) # write text of page
+        #out.write(bytes((12,))) # write page delimiter (form feed 0x0C)
+    #out.close()
+    doc.close()
+
     print(f"{doc_name}.pdf extracted to text :)")
+    
+    return extracted_text
---
+++ utils/process_nlp.py
@@ -0,0 +1,24 @@
+from decouple import config
+from llama_index.llms.gemini import Gemini
+from llama_index.core.llms import ChatMessage
+from extract_text import extract_text
+
+
+GOOGLE_API_KEY= config('GOOGLE_API_KEY')
+llm = Gemini(api_key=GOOGLE_API_KEY, model_name="models/gemini-pro")
+
+
+def answer_text_question(extracted_text: str, question: str) -> str:
+
+    messages = [
+    ChatMessage(
+        role="system", content=extracted_text
+    ),
+    ChatMessage(role="user", content=question),
+    ]
+    response = llm.chat(messages)
+
+    return response
+
+
+#answer_text_question(extract_text('input_data/sample.pdf'),  "how many years experience do you think i have?")
